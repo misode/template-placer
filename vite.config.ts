65e2152be6060459d04764dea75a747da5508b0f
Misode <Misoloo64@gmail.com>
2022-05-19 13:51:40 +0200

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/structure-placer/',
+	base: '/template-placer/',
 	plugins: [preact()],
 })
